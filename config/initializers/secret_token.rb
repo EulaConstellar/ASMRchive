b2af258b3455bcdbb16a3b3078436f1d37ef6aa8
EulaConstellar <jessica-elliot@hotmail.com>
2015-07-22 20:12:30 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ASMRchive::Application.config.secret_key_base = 'bde3c8fa8226becc8d430f26684d98701abc1a1a8e835299887e7b755416da62b983acb52adf8954b43e67872cd4b0e054f0f15313c0ed2af3bdb320824a981e'
